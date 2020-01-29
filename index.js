46c1a8c386c068b68617b0de32d4afb294119450
Chris Padilla <cdpadilla42@gmail.com>
2020-01-29 15:34:08 -0600

    add successful fetch

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-fetch('api.openweathermap.org/data/2.5/weather?q=London', { mode: 'cors' }).then(result => {
-  console.log(result);
-});
+fetch(
+  'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=3071d7049f77d2a60a294f98fadfa6f3',
+  {
+    mode: 'cors'
+  }
+)
+  .then(result => {
+    return result.json();
+  })
+  .then(result => {
+    console.log(result);
+  });
+
+// API Key:
+// 3071d7049f77d2a60a294f98fadfa6f3
+
+// Grab:
+// Main (sunny, cloudy, rain)
+// Temperature (in Kelvin first)
+// Precipitation: %
+// Funny picture based off main haha
